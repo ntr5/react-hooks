890b27a19f733182494810912f6477df76ad4beb
ntr5 <pressntr@gmail.com>
2020-07-28 19:15:36 -0600

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import Form from './components/Form';
+import People from './components/People';
+import NewestPerson from './components/NewestPerson';
+
+const App = () => {
+  const [people, setPeople] = useState([
+    {
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Doe'
+    }
+  ]);
+  const addPerson = (person) => {
+    setPeople([...people, person]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="row">
+        
+        <Form addPerson={addPerson}/>
+        <People people={people}/>
+        <NewestPerson newestPerson={people[people.length -1]}/>
+      </div>
     </div>
   );
 }
