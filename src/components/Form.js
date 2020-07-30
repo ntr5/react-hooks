53319a94e7c71958b24763569b94ff410010e892
ntr5 <pressntr@gmail.com>
2020-07-29 18:38:37 -0600

    using useMemo and useRef

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,59 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useMemo, useRef } from 'react';
 import PeopleContext from '../context/peopleContext';
 
 const Form = () => {
   const [person, setPerson] = useState({ firstName: '', lastName: ''});
-  const context = useContext(PeopleContext)
+  const context = useContext(PeopleContext);
 
-    const onChange = event => {
-        setPerson({ ...person, [event.target.name]: event.target.value})
-      }
-      const onSubmit = event => {
-        event.preventDefault();
-        if(person.firstName.trim() === '' || person.lastName.trim() === '') return;
-    
-        const newPerson = {
-          firstName: person.firstName.trim(),
-          lastName: person.lastName.trim(),
-        };
-    
-        
-        context.addPerson(newPerson);
-        setPerson({ firstName: '', lastName: '' })
-      }
-    return (
-        <div className="col">
-          <h2>Add a person: </h2>
-          <hr/>
-          <form onSubmit={onSubmit}>
+  const firstNameInput = useRef(null);
+
+  const onChange = event => {
+    setPerson({ ...person, [event.target.name]: event.target.value})
+  }
+  const onSubmit = event => {
+    event.preventDefault();
+    if(person.firstName.trim() === '' || person.lastName.trim() === '') return;
+
+    const newPerson = {
+      firstName: person.firstName.trim(),
+      lastName: person.lastName.trim(),
+    };
+
+    context.addPerson(newPerson);
+    setPerson({ firstName: '', lastName: '' });
+    firstNameInput.current.focus();
+  }
+  const printNumberOfPeople = () => console.log(`Number of people: ${context.people.length}`);
+  useMemo(() => printNumberOfPeople(), []);
+  return (
+      <div className="col">
+        <h2>Add a person: </h2>
+        <hr/>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="text"
+              className="form-control"
+              name="firstName"
+              placeholder="First Name..."
+              value={person.firstName}
+              ref={firstNameInput}
+              onChange={onChange}
+            />
             <div className="form-group">
-              <input 
-                type="text"
-                className="form-control"
-                name="firstName"
-                placeholder="First Name..."
-                value={person.firstName}
-                onChange={onChange}
-              />
-              <div className="form-group">
-              <input 
-                type="text"
-                className="form-control"
-                name="lastName"
-                placeholder="Last Name..."
-                value={person.lastName}
-                onChange={onChange}
-              />
-              </div>
-              <button className="btn btn-success" type="submit">Add person</button>
+            <input 
+              type="text"
+              className="form-control"
+              name="lastName"
+              placeholder="Last Name..."
+              value={person.lastName}
+              onChange={onChange}
+            />
             </div>
-          </form>
-        </div>
+            <button className="btn btn-success" type="submit">Add person</button>
+          </div>
+        </form>
+      </div>
     )
 }
 
